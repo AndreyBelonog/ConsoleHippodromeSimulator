6b3d2c0c54608f9366a5b91f48bc88bd1449ef29
AndreyBelonog <???@users.noreply.github.com>
2018-03-13 17:28:41 +0200

    Added Horse class; Added private field to Hippodrome.

--- src/com/company/Hippodrome.java
+++ src/com/company/Hippodrome.java
@@ -1,6 +1,13 @@
 package com.company;
 
+import java.util.List;
+
 public class Hippodrome {
+    private List<Horse> horses;
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
 
     public static void main(String[] args) {
 	// write your code here
