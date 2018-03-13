322e8c8d8f100cf96c906c6aa581652aeecf6545
AndreyBelonog <???@users.noreply.github.com>
2018-03-13 17:43:51 +0200

    Added methods to Hippodrome; Implemented "run()" method.

--- src/com/company/Hippodrome.java
+++ src/com/company/Hippodrome.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
     private List<Horse> horses;
+    static Hippodrome game;
 
     public List<Horse> getHorses() {
         return horses;
@@ -13,7 +15,32 @@ public class Hippodrome {
         this.horses = horses;
     }
 
+    private void move(){
+
+    }
+
+    private void print(){
+
+    }
+
+    private void run(){
+        for(int i = 0; i < 100; i++){
+            move();
+            print();
+            try{
+                Thread.sleep(200);
+            }catch (InterruptedException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        game = new Hippodrome(new ArrayList<>());
+        game.horses.add(new Horse("First", 3,0));
+        game.horses.add(new Horse("Second", 3,0));
+        game.horses.add(new Horse("Third", 3,0));
+        game.horses.add(new Horse("Fourth", 3,0));
+        game.horses.add(new Horse("Fifth", 3,0));
     }
 }
